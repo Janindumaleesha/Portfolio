49db06643d7df2c8430b81a5e925904a45b8ea2f
Janindumaleesha <janindu.maleesha@app360.lk>
2025-06-24 23:44:20 +0530

    create manu not complete

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './sections/Hero'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
   )
 }
 
